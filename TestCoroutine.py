64fe559a2a1e0ae9d0ad723caf75dc43bad76a32
todd136 <tao124@gmail.com>
2016-05-16 14:58:45 +0800

    test coroutine

---
+++ TestCoroutine.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+# -*- encoding:utf-8 -*-
+
+import time
+
+def consumer():
+    r = ''
+    while True:
+        n = yield r
+        if not n:
+            return
+        print('[Consumer] consuming %s...' % n)
+        # time.sleep(2)
+        r = '200 OK'
+
+def produce(c):
+    c.send(None)
+    n = 0
+    while n < 5:
+        n = n+1
+        print('[Producer] Producing %s ...' % n)
+        r = c.send(n)
+        print('[Producer] Consumer returns: %s' % r)
+    c.close()
+
+c = consumer()
+produce(c)
